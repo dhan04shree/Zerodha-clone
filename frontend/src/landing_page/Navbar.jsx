c295fb5a2a59a9d034c8871c0c2005baa46a09ec
dhan04shree <dhanshreemagar0@gmail.com>
2025-03-21 23:57:36 +0530

    frontend changes

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,23 +1,42 @@
 import { Link } from "react-router-dom";
-export default function Navbar(){
-    return(
+export default function Navbar() {
+  return (
     <nav className="navbar navbar-expand-lg border-bottom">
-        <div className="container p-2">
-            <Link className="navbar-brand"  to={"/"}><img src="images/logo.svg" style={{width:"25%"}} alt="logo" /></Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <Link className="nav-link f-link" to={"/signup"}>Signup</Link>
-                    <Link className="nav-link f-link" to={"/about"}>About</Link>
-                    <Link className="nav-link f-link" to={"/products"}>Products</Link>
-                    <Link className="nav-link f-link" to={"/pricing"}>Pricing</Link>
-                    <Link className="nav-link f-link" to={"/support"}>Support</Link>
-                    <Link className="nav-link" href="#"><i className="fa-solid fa-bars"></i></Link>
-                </div>
-            </div>
+      <div className="container p-2">
+        <Link className="navbar-brand" to={"/"}>
+          <img src="images/logo.svg" style={{ width: "25%" }} alt="logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link className="nav-link f-link" to={"/signup"}>
+              Signup
+            </Link>
+            <Link className="nav-link f-link" to={"/about"}>
+              About
+            </Link>
+            <Link className="nav-link f-link" to={"/products"}>
+              Products
+            </Link>
+            <Link className="nav-link f-link" to={"/pricing"}>
+              Pricing
+            </Link>
+            <Link className="nav-link f-link" to={"/support"}>
+              Support
+            </Link>
+            <Link className="nav-link" href="#">
+              <i className="fa-solid fa-bars"></i>
+            </Link>
+          </div>
         </div>
+      </div>
     </nav>
-    );
+  );
 }
