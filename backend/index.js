5aaeb20b5540743ffd7a641209bf2f7a9981185d
dhan04shree <dhanshreemagar0@gmail.com>
2025-05-05 13:18:52 +0530

    Add API endpoints to fetch all holdings and positions

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,34 @@ require('dotenv').config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const app = express();
 
 const PORT = process.env.PORT || 3002
 const DBURL = process.env.MONGO_URL;
-
 mongoose.connect(DBURL);
 
-const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+const {HoldingModel} = require("./model/HoldingModel");
+const {PositionsModel} = require("./model/PositionsModel");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/",(req,res)=>{
+    res.send("hii");
+})
+app.get("/allHoldings",async(req,res)=>{
+    let allHoldings = await HoldingModel.find({});
+    res.json(allHoldings);
+    
+})
+app.get("/allPositions",async(req,res)=>{
+    let allPositions = await PositionsModel.find({});
+    res.json(allPositions);
+    
+})
 app.listen(PORT,()=>{
-    console.log("DB connected");
+    console.log("app listeninig to port",PORT);
 });
