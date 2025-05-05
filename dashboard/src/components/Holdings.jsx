3038b1e9519bfc368d72ebeb82ef9bc07d09a232
dhan04shree <dhanshreemagar0@gmail.com>
2025-05-05 13:33:04 +0530

    Load holdings data from DB instead of static file

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -2,27 +2,27 @@ import { useState,useEffect } from "react";
 import axios, { all } from "axios";
 import VerticalGraph from "./VerticalGraph";
 
-import { holdings } from "../data/data"
+// import { holdings } from "../data/data"
 
 export default function Holdings(){
-  // const [allHoldings, setAllHoldings] = useState([]);
+  const [allHoldings, setAllHoldings] = useState([]);
 
-  // useEffect(() => {
-  //   axios.get("http://localhost:5173/allHoldings").then((res) => {
-  //     // console.log(res.data);
-  //     console.log("API response:", res.data); 
-  //     // setAllHoldings(res.data);
-  //   });
-  // }, []);
+  useEffect(() => {
+    axios.get("http://localhost:3002/allHoldings").then((res) => {
+      // console.log(res.data);
+      console.log("API response:", res.data); 
+      setAllHoldings(res.data);
+    });
+  }, []);
 
-  const labels = holdings.map((subArray) => subArray["name"]);
+  const labels = allHoldings.map((subArray) => subArray["name"]);
 
   const data = {
     labels,
     datasets: [
       {
         label: "Stock Price",
-        data: holdings.map((stock) => stock.price),
+        data: allHoldings.map((stock) => stock.price),
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
     ],
@@ -30,7 +30,7 @@ export default function Holdings(){
 
   return (
     <>
-      <h3 className="title">Holdings ({holdings.length})</h3>
+      <h3 className="title">Holdings ({allHoldings.length})</h3>
 
       <div className="order-table">
         <table>
@@ -45,7 +45,7 @@ export default function Holdings(){
             <th>Day chg.</th>
           </tr>
 
-          {holdings.map((stock, index) => {
+          {allHoldings.map((stock, index) => {
             const curValue = stock.price * stock.qty;
             const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
