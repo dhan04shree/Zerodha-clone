54df395b831c0d2b7d5b7dd878d99140b6549aab
dhan04shree <dhanshreemagar0@gmail.com>
2025-05-12 18:06:54 +0530

    feat(auth)- jwt auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import HomePage from "./landing_page/home/HomePage"
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Signup from "./landing_page/signup/Signup"
+import Login from "./landing_page/signup/Login"
 import AboutPage from "./landing_page/about/AboutPage"
 import PricingPage from "./landing_page/pricing/PricingPage"
 import ProductPage from "./landing_page/products/ProductPage"
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
         <Route path="/about" element={<AboutPage/>}></Route>
         <Route path="/pricing" element={<PricingPage/>}></Route>
         <Route path="/products" element={<ProductPage/>}></Route>
--- frontend/src/landing_page/home/HomePage.jsx
+++ frontend/src/landing_page/home/HomePage.jsx
@@ -1,10 +1,48 @@
 import Pricing from "./Pricing";
 import Hero from "./Hero";
-import Awards from "./Awards";
 import Stats from "./Stats";
 import Education from "./Education";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import OpenAccount from "../OpenAccount";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function HomePage(){
+
+    const navigate = useNavigate();
+    const [cookies, removeCookie] = useCookies([]);
+    const [username, setUsername] = useState("");
+    useEffect(() => {
+        const verifyCookie = async () => {
+            if (!cookies.token) {
+            navigate("/login");
+        }
+        const { data } = await axios.post(
+            "http://localhost:3002",
+            {},
+        { withCredentials: true }
+        );
+        const { status, user } = data;
+        setUsername(user);
+        return status
+        ? toast(`Hello ${user}`, {
+            position: "top-right",
+          })
+        : (removeCookie("token"), navigate("/login"));
+    };
+
+
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
+
+  const Logout = () => {
+    removeCookie("token");
+    navigate("/signup");
+  };
+
     return(
         <>
         <Hero/>
@@ -13,7 +51,7 @@ export default function HomePage(){
         <Education/>
         {/* <Awards/> */}
         <OpenAccount/>
-       
+       <ToastContainer />
         </>
     );
 }
